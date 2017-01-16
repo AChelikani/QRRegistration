9111d7ee94d531927bd144f6dfb1da342cc6e56c
Advith Chelikani <advith.chelikani@gmail.com>
2017-01-15 17:19:52 -0800

    qr code creation functional

--- qrwrite.py
+++ qrwrite.py
@@ -3,19 +3,20 @@ import pyqrcode
 qr = pyqrcode.create("Test")
 qr.png("test.png", scale=6)
 
-class QRCode(object):
-    def __init__(self, id):
-        self.id = id
+class QRCodeCreator(object):
+    def __init__(self):
+        pass
 
     def makeQR(self, contents, scale, filename):
         qr = pyqrcode.create(contents)
         qr.png(filename, scale=scale)
 
-    def createContents(self):
-        return self.id
+    def createContents(self, id):
+        return id
 
-    def createQR(self):
-        self.makeQR(createContents(), 6, str(self.id)+".png")
+    def createQR(self, id):
+        self.makeQR(self.createContents(id), 6, str(id)+".png")
 
 if __name__ == "__main__":
-    pass
+    qr = QRCodeCreator()
+    qr.createQR(2)
